6d94d23cc271ad6a7d0015097059dab17086a3d4
ValeriaPostylyakova <valeria.postylyakova@mail.ru>
2026-06-25 21:47:05 +0300

    поставила debug в false

--- config/settings.py
+++ config/settings.py
@@ -14,7 +14,7 @@ load_dotenv()
 
 
 SECRET_KEY = os.getenv("SECRET_KEY")
-DEBUG = os.getenv("DEBUG")
+DEBUG = False
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '.render.com localhost 127.0.0.1').split(' ')
 
