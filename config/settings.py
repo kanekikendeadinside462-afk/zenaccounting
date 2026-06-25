08591a88f78358d0264b0545594dfb60c94b923c
ValeriaPostylyakova <valeria.postylyakova@mail.ru>
2026-06-25 16:09:06 +0300

    настройка debug режима

--- config/settings.py
+++ config/settings.py
@@ -14,7 +14,7 @@ load_dotenv()
 
 
 SECRET_KEY = os.getenv("SECRET_KEY")
-DEBUG = False
+DEBUG = os.getenv("DEBUG")
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '.render.com localhost 127.0.0.1').split(' ')
 
